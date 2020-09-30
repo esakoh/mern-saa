cf6b0bed9fb94060410275d005625db0535a653b
Esa Kohtala <Esa.Kohtala@seamk.fi>
2020-09-30 08:22:41 +0300

    commit

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const server = createServer(app);
 
 mongoose.set('useFindAndModify', false);
 
+app.use(express.static(__dirname+'/build'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client','public','index.html'));
